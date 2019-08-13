f530dab05868c39116f4ae1df9f13e4fd074fda8
John Otu <johnne01@gmail.com>
2019-08-14 00:01:59 +0100

    added logging

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const winston = require('./winston');
 
 require('dotenv').config();
 
@@ -13,5 +15,13 @@ const port = 3300 || process.env.PORT;
 app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
+app.use(morgan('combined', { stream: winston.stream }));
+
+
+
+app.use((err, req, res, next) => {
+  winston.error(`${err.status || 500} - ${req.method} - ${err.message} - ${req.originalUrl} - ${req.ip}`);
+  res.status(err.status || 500).json({ msg: 'An error has occured' });
+})
 
 app.listen(port, () => console.log('Discounter server running on port', port));
