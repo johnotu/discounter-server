cd0a6ade1ac08e2e8bcf956de495ca3ce88f8946
John Otu <johnne01@gmail.com>
2019-08-15 14:19:33 +0100

    fixed eslint issues with mocha
    added mocha env rule so eslint can recognize global test variables

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const assert = require('assert');
-const expect = require('chai').expect;
+// const expect = require('chai').expect;
 const call = require('supertest');
 const app = require('../index');
 
