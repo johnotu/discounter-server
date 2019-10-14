c34da2863bcdabc45d3bbcb361a4e726d08918a8
John Otu <johnne01@gmail.com>
2019-10-14 02:47:19 +0100

    Fix typo
    Corrected loginAccount function call

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const createAccount = require('./functions/account/create-account');
 const getAccount = require('./functions/account/get-account');
 const updateAccount = require('./functions/account/update-account');
 const deleteAccount = require('./functions/account/delete-account');
-const loginAcount = require('./functions/account/login-account');
+const loginAccount = require('./functions/account/login-account');
 
 exports.createAccount = (req, res) => createAccount(req, res);
 exports.getAccount = (req, res) => getAccount(req, res);
 exports.updateAccount = (req, res) => updateAccount(req, res);
 exports.deleteAccount = (req, res) => deleteAccount(req, res);
-exports.loginAcount = (req, res) => loginAcount(req, res);
+exports.loginAccount = (req, res) => loginAccount(req, res);
